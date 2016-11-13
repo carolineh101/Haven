f5b8b9240df3180034834dbc648aa9ff02030074
carolineh101 <cho19@stanford.edu>
2016-11-12 23:09:11 -0800

    Changes hospital markers to custom drawable.

--- app/src/main/java/com/example/carolineho/haven/MapsActivity.java
+++ app/src/main/java/com/example/carolineho/haven/MapsActivity.java
@@ -22,6 +22,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -156,6 +157,7 @@ public class MapsActivity extends AppCompatActivity implements
                                 jsonObj.getJSONArray("latlng").getDouble(1)
                         ))
                         .title(jsonObj.getString("name"))
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.hospitals))
                 );
             }
         } catch (JSONException e) {
